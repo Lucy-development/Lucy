e0ff4415fb514f742c04218fb14fee0b2fed9d80
Priit Paluoja <priitpaluoja@gmail.com>
2016-02-16 19:16:57 +0200

    Hello World!

--- core/src/main/java/test/MainTest.java
+++ core/src/main/java/test/MainTest.java
@@ -1,7 +1,12 @@
 package test;
 
+import static spark.Spark.get;
+
 /**
  * Created by Kaspar on 15/02/2016.
  */
 public class MainTest {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World - LUCY");
+    }
 }
