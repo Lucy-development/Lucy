e799bf377b0c9ad1dd441468cc683b21ec5f9a01
Priit Paluoja <priitpaluoja@gmail.com>
2016-03-01 23:07:33 +0200

    Implemented SentMessage.
     ID is by default -1. RuntimeException will be thrown if getID() is called AND id == -1.

--- core/src/main/java/data/SentMessage.java
+++ core/src/main/java/data/SentMessage.java
@@ -7,30 +7,48 @@ import java.sql.Timestamp;
  * Created on 25/02/2016.
  */
 public class SentMessage implements Sendable {
-    // TODO: implement
+    private Integer id = -1;
+    private Timestamp time;
+    private Person sender;
+    private Person receiver;
+    private String meta;
+    private String content;
+
+    public SentMessage(Timestamp time, Person sender, Person receiver, String meta, String content) {
+        this.time = time;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.meta = meta;
+        this.content = content;
+    }
 
     @Override
     public Integer getID() {
-        return null;
+        if (id == -1) throw new RuntimeException("ID not assigned!");
+        return id;
     }
 
     @Override
     public Timestamp getTime() {
-        return null;
+        return time;
     }
 
     @Override
     public Person getSender() {
-        return null;
+        return sender;
     }
 
     @Override
     public Person getReceiver() {
-        return null;
+        return receiver;
     }
 
     @Override
     public String getMeta() {
-        return null;
+        return meta;
+    }
+
+    public String getContent() {
+        return content;
     }
 }
