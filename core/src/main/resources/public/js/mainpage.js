6d674f2a6ba75338fed987c372f47b2c0b5010b1
MariLiis <mariliisvelner@gmail.com>
2016-03-10 13:26:19 +0200

    Renamed settings.js to mainpage.js - JavaScript for all main page elements. Send button location now more or less changes according to window size.

--- core/src/main/resources/public/js/mainpage.js
+++ core/src/main/resources/public/js/mainpage.js
@@ -12,3 +12,18 @@ function displaySettings(){
 
     }
 }
+window.addEventListener("resize",sendbuttonlocation);
+
+//TODO: could be better
+function sendbuttonlocation(){
+    var w = window.innerWidth;
+    var c = Math.floor((1640 - w)/150);
+    var p = 25 - 0.5*c;
+    if (c == 4 || c == 5){
+        p = 25 - 0.66*c;
+    }
+    if (c > 5){
+        p = 25 - c*(0.8+(c-6)*0.27);
+    }
+    document.getElementById("send").style.right = String(p)+"%";
+}
