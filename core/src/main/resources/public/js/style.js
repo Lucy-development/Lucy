8b1f7c68b23dbaafb5970c2037c368b3fd4cd196
MariLiis <mariliisvelner@gmail.com>
2016-04-22 13:46:35 +0300

    Removed Chat and Map from main page and changed searchcontact placeholder.

--- core/src/main/resources/public/js/style.js
+++ core/src/main/resources/public/js/style.js
@@ -1,10 +1,19 @@
-function changeStyle(style){
-    if (style === "default"){
+
+function changeStyle(style) {
+    if (style === "default") {
         document.getElementById('wrapper').className = "";
         document.getElementById('messagebox').className = "";
+        var all = document.getElementsByClassName('message_location_darcula');
+        for (var i = 0; i < all.length; i++) {
+            all[i].className = "message_location";
+        }
     }
-    else{
+    else {
         document.getElementById('wrapper').className = 'wrapper_darcula';
         document.getElementById('messagebox').className = "messagebox_darcula";
+        var all = document.getElementsByClassName('message_location');
+        for (var i = 0; i < all.length; i++) {
+            all[i].className = "message_location_darcula";
+        }
     }
 }
