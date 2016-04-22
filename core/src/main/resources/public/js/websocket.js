5be28e71b6873225482c3170e8b169d9ea636320
kasparpapli <kaspar.papli@gmail.com>
2016-04-22 13:26:05 +0300

    Added client-side support for fetching message history & prototype

--- core/src/main/resources/public/js/websocket.js
+++ core/src/main/resources/public/js/websocket.js
@@ -1,8 +1,11 @@
 var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat/");
 var connectionOpen = true;
 var sessionAuthenticated = false;
+
 var myLid = getCookie("myLid");
 var myName = getCookie("myName");
+
+var messageCount = 0;
 
 
 // WS HANDLERS
@@ -28,13 +31,19 @@ webSocket.onclose = function (closeEvent) {
 
 // EVENT LISTENERS
 elementById("send").addEventListener("click",
-    sendHandler
+    sendMessageHandler
 );
 elementById("message").addEventListener("keypress", function (key) {
     if (key.keyCode === 13) {
-        sendHandler();
+        sendMessageHandler();
     }
 });
+var msgBox = document.getElementById("messagebox");
+msgBox.onscroll = function () {
+    if (msgBox.scrollTop == 0) {
+        sendHistoryRequest();
+    }
+};
 
 
 function messageHandler(string) {
@@ -61,13 +70,15 @@ function messageHandler(string) {
             writeContactHtmlTag(senderLid, sender);
         }
 
-
         if (location == null) {
             insertToMessageBox(composeRegularMessage(senderLid, messageContent, sender));
         }
         else {
             insertToMessageBox(composeRegularMessage(senderLid, messageContent, sender, location));
         }
+
+        messageCount++;
+
     } else if (purpose === "auth_resp") {
         if (status === "success") {
             sessionAuthenticated = true;
@@ -84,20 +95,29 @@ function messageHandler(string) {
             throw "Received init_contacts response with status: " + status;
         }
 
+    } else if (purpose === 'hist_response') {
+        if (status === "success") {
+            handleHistoryResponse(serverMessage);
+        } else {
+            insertToMessageBox(composeLogMessage("Failed to fetch history"));
+            throw "Received hist_response response with status: " + status;
+        }
+
     } else {
         throw "Unexpected purpose for message: " + string;
     }
 }
 
-function sendHandler() {
+function sendMessageHandler() {
     var msgContent = getMsgInput();
     var receiver = getCurrentlyActiveReceiver();
     if (msgContent !== "") {
         if (connectionOpen) {
             if (sessionAuthenticated) {
-                sendMessage(msgContent, receiver);
+                sendMessage(msgContent, "msg", receiver);
                 clearMsgInput();
                 insertToMessageBox(composeRegularMessage(lidToContactName(myLid), msgContent, "me", ""));
+                messageCount++;
             } else {
                 insertToMessageBox(composeLogMessage("Unable to send message, WebSocket session is unauthenticated"));
             }
@@ -107,186 +127,204 @@ function sendHandler() {
     }
 }
 
-function handleContactResponse(jsonObj) {
-    writeContacts(jsonObj.contacts);
-}
+function sendHistoryRequest() {
+    var msgContent = messageCount;
 
-/**
- * Method inserts html tag into the index.html
- */
-function writeContactHtmlTag(id, contactName) {
-    //This code creates a new <li> element:
-    var para = document.createElement('li');
-    //Set contact id for the element
-    para.id = id;
-
-    para.setAttribute("onclick", "changeSelectedContactClass(" + "'" + id + "'" + ");");
-
-    para.className = 'deactivated';
-
-    //To add text to the <el> element, you must create a text node first. This code creates a text node:
-    var node = document.createTextNode(contactName);
-
-    //Then you must append the text node to the <ul> element:
-    para.appendChild(node);
-
-    //Finally you must append the new element to an existing element.
-
-    //This code finds an existing element:
-    var element = document.getElementById('list');
-    //This code appends the new element to the existing element:
-    element.appendChild(para);
-}
-
-
-/**
- *
- * @param contacts - list of contacts that will be written to the HTML
- */
-function writeContacts(contacts) {
-    for (var i = 0; i < contacts.length; i++) {
-        writeContactHtmlTag(contacts[i].lid, contacts[i].fname + " " + contacts[i].lname);
+    if (connectionOpen) {
+        if (sessionAuthenticated) {
+            sendMessage(msgContent, "hist", "");
+        }
     }
 }
 
-/**
- * Method changes chosen contact class
- */
-function changeSelectedContactClass(id) {
-    var activated = document.getElementsByClassName('activated');
-    var index, len;
-    for (index = 0, len = activated.length; index < len; ++index) {
-        activated[index].setAttribute('class', 'deactivated');
+    function handleContactResponse(jsonObj) {
+        writeContacts(jsonObj.contacts);
     }
 
-    var el = document.getElementById(String(id));
-    console.log(id);
+    /**
+     * Method inserts html tag into the index.html
+     */
+    function writeContactHtmlTag(id, contactName) {
+        //This code creates a new <li> element:
+        var para = document.createElement('li');
+        //Set contact id for the element
+        para.id = id;
 
-    // Check that elements exists before editing
-    if (el) {
-        el.setAttribute('class', 'activated');
+        para.setAttribute("onclick", "changeSelectedContactClass(" + "'" + id + "'" + ");");
+
+        para.className = 'deactivated';
+
+        //To add text to the <el> element, you must create a text node first. This code creates a text node:
+        var node = document.createTextNode(contactName);
+
+        //Then you must append the text node to the <ul> element:
+        para.appendChild(node);
+
+        //Finally you must append the new element to an existing element.
+
+        //This code finds an existing element:
+        var element = document.getElementById('list');
+        //This code appends the new element to the existing element:
+        element.appendChild(para);
     }
-}
 
 
-function sendAuthSessionRequest(sessionKey) {
-    var authRequestObject = {};
-    authRequestObject.purpose = "auth";
-    authRequestObject.content = sessionKey;
-    webSocket.send(JSON.stringify(authRequestObject));
-}
+    /**
+     *
+     * @param contacts - list of contacts that will be written to the HTML
+     */
+    function writeContacts(contacts) {
+        for (var i = 0; i < contacts.length; i++) {
+            writeContactHtmlTag(contacts[i].lid, contacts[i].fname + " " + contacts[i].lname);
+        }
+    }
+
+    function handleHistoryResponse(historyResponse) {
+        for (var i = 0; i < historyResponse.messages.length; i++) {
+            // TODO: add new messages; scroll
+            console.log(historyResponse.messages[i].content);
+            // historyResponse.messages[i].sender
+        }
+    }
+
+    /**
+     * Method changes chosen contact class
+     */
+    function changeSelectedContactClass(id) {
+        var activated = document.getElementsByClassName('activated');
+        var index, len;
+        for (index = 0, len = activated.length; index < len; ++index) {
+            activated[index].setAttribute('class', 'deactivated');
+        }
+
+        var el = document.getElementById(String(id));
+        console.log(id);
+
+        // Check that elements exists before editing
+        if (el) {
+            el.setAttribute('class', 'activated');
+        }
+    }
 
 
-function sendMessage(message, receiver) {
-    var messageObject = {};
-    messageObject.purpose = "msg";
-    messageObject.to = receiver;
-    messageObject.content = message;
+    function sendAuthSessionRequest(sessionKey) {
+        var authRequestObject = {};
+        authRequestObject.purpose = "auth";
+        authRequestObject.content = sessionKey;
+        webSocket.send(JSON.stringify(authRequestObject));
+    }
 
-    //TODO: we are dependent of Google
-    // IMPORTANT: the following is async and uses callbacks! Edit with caution!
-    // Insert geolocation data
-    // Check if browser supports geolocation
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            //First function is success function
-            function (position) {
-                //Get the latitude and the longitude;
-                messageObject.latitude = position.coords.latitude;
-                messageObject.longitude = position.coords.longitude;
-                //Google service for reverse geocoding: we can get address by coordinates. Note, it is asyinc
 
-                var geocoder = new google.maps.Geocoder();
-                var latlng = new google.maps.LatLng(messageObject.latitude, messageObject.longitude);
-                geocoder.geocode({'latLng': latlng}, function (results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        // Use 0 for more precision
-                        if (results[1]) messageObject.location = results[1].formatted_address;
-                    }
+    function sendMessage(message, purpose, receiver) {
+        var messageObject = {};
+        messageObject.purpose = purpose;
+        messageObject.to = receiver;
+        messageObject.content = message;
+
+        //TODO: we are dependent of Google
+        // IMPORTANT: the following is async and uses callbacks! Edit with caution!
+        // Insert geolocation data
+        // Check if browser supports geolocation
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                //First function is success function
+                function (position) {
+                    //Get the latitude and the longitude;
+                    messageObject.latitude = position.coords.latitude;
+                    messageObject.longitude = position.coords.longitude;
+                    //Google service for reverse geocoding: we can get address by coordinates. Note, it is asyinc
+
+                    var geocoder = new google.maps.Geocoder();
+                    var latlng = new google.maps.LatLng(messageObject.latitude, messageObject.longitude);
+                    geocoder.geocode({'latLng': latlng}, function (results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            // Use 0 for more precision
+                            if (results[1]) messageObject.location = results[1].formatted_address;
+                        }
+                        webSocket.send(JSON.stringify(messageObject));
+                    });
+                },
+                // Second function is for case where geolocation fails
+                function () {
                     webSocket.send(JSON.stringify(messageObject));
                 });
-            },
-            // Second function is for case where geolocation fails
-            function () {
-                webSocket.send(JSON.stringify(messageObject));
-            });
-    } else {
-        // When browser does not support geolocating
-        webSocket.send(JSON.stringify(messageObject));
+        } else {
+            // When browser does not support geolocating
+            webSocket.send(JSON.stringify(messageObject));
+        }
     }
-}
 
-/**
- * Avoid inventing the wheel? (http://www.w3schools.com/js/js_cookies.asp)
- * TODO: invent the wheel
- */
-function getCookie(cookieName) {
-    var name = cookieName + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    /**
+     * Avoid inventing the wheel? (http://www.w3schools.com/js/js_cookies.asp)
+     * TODO: invent the wheel
+     */
+    function getCookie(cookieName) {
+        var name = cookieName + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
     }
-    return "";
-}
 
 
-/**
- * Method returns the id of the currently chosen recipient
- */
-function getCurrentlyActiveReceiver() {
-    if (getReceiverBoxId() !== "") {
-        return getReceiverBoxId();
-    } else {
-        if (elementByClass("activated").length === 1)
-            return elementByClass("activated")[0].getAttribute("id");
-        //TODO: Error handling
+    /**
+     * Method returns the id of the currently chosen recipient
+     */
+    function getCurrentlyActiveReceiver() {
+        if (getReceiverBoxId() !== "") {
+            return getReceiverBoxId();
+        } else {
+            if (elementByClass("activated").length === 1)
+                return elementByClass("activated")[0].getAttribute("id");
+            //TODO: Error handling
+        }
     }
-}
 
 
-function lidToContactName(lid) {
-    // TODO: return corresponding contact name
-    return lid;
-}
+    function lidToContactName(lid) {
+        // TODO: return corresponding contact name
+        return lid;
+    }
 
 
-function composeRegularMessage(senderLid, messageContent, sender, location) {
-    if (senderLid === myLid)
-        return "<div class='outgoing'>" + myName + ": " + messageContent + "</div>";
-    return "<div class='incoming'>" + sender + ": " + messageContent + "</div>" + "<div class='message_location'>"+'Near: ' + location + "</div>";
-}
+    function composeRegularMessage(senderLid, messageContent, sender, location) {
+        if (senderLid === myLid)
+            return "<div class='outgoing'>" + myName + ": " + messageContent + "</div>";
+        return "<div class='incoming'>" + sender + ": " + messageContent + "</div>" + "<div class='message_location'>" + 'Near: ' + location + "</div>";
+    }
 
-function composeLogMessage(logMessage) {
-    return "<div class='logmessage'>" + logMessage + "</div>";
-}
+    function composeLogMessage(logMessage) {
+        return "<div class='logmessage'>" + logMessage + "</div>";
+    }
 
-function getMsgInput() {
-    return elementById("message").value;
-}
+    function getMsgInput() {
+        return elementById("message").value;
+    }
 
-function clearMsgInput() {
-    elementById("message").value = "";
-}
+    function clearMsgInput() {
+        elementById("message").value = "";
+    }
 
-function getReceiverBoxId() {
-    return elementById("searchcontact").value;
-}
+    function getReceiverBoxId() {
+        return elementById("searchcontact").value;
+    }
 
-function insertToMessageBox(string) {
-    insertBeforeEnd("messagebox", string);
-}
+    function insertToMessageBox(string) {
+        insertBeforeEnd("messagebox", string);
+    }
 
-function insertBeforeEnd(targetId, string) {
-    elementById(targetId).insertAdjacentHTML("beforeend", string);
-}
+    function insertBeforeEnd(targetId, string) {
+        elementById(targetId).insertAdjacentHTML("beforeend", string);
+    }
 
-function elementById(id) {
-    return document.getElementById(id);
-}
+    function elementById(id) {
+        return document.getElementById(id);
+    }
 
-function elementByClass(className) {
-    return document.getElementsByClassName(className);
-}
+    function elementByClass(className) {
+        return document.getElementsByClassName(className);
+    }
 
